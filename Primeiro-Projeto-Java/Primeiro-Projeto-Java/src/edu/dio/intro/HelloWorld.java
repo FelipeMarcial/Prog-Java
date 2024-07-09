e70d4eb4bbcc97b8eda25e667f3ba42641203d82
FelipeMarcial <felipe.mt@outlook.com>
2024-07-08 22:45:47 -0300

    Adição de código para concatenar nome e testar métodos

--- Primeiro-Projeto-Java/Primeiro-Projeto-Java/src/edu/dio/intro/HelloWorld.java
+++ Primeiro-Projeto-Java/Primeiro-Projeto-Java/src/edu/dio/intro/HelloWorld.java
@@ -1,4 +1,7 @@
-public class App {
+package edu.dio.intro;
+
+public class HelloWorld {
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
